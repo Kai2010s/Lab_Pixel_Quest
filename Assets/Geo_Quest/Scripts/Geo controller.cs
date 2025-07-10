4ae4fb7f2f23232916b676da9159c83929fb1b61
Kai2010s <kais53@nycstudents.net>
2025-07-10 12:18:27 -0400

    Day 4

--- Assets/Geo_Quest/Scripts/Geo controller.cs
+++ Assets/Geo_Quest/Scripts/Geo controller.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Geocontroller : MonoBehaviour
 {
     string Bye = "How are you?";
     int number1 = 1;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
+    public int speed = 5;
     void Start()
-
     {
+        rb = GetComponent<Rigidbody2D>();
         string goat = "somewhere";
         Debug.Log("Hello world");
         Debug.Log(Bye + goat);
@@ -19,7 +23,15 @@ public class Geocontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(number1);
-        number1++;
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
 }
+      
+    
